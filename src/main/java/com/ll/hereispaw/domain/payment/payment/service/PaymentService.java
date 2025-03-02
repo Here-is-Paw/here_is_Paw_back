8690c98cae86b2c74fb1feee9fe3369be322a388
seonghyeok <chosh2001@naver.com>
2025-03-02 19:19:23 +0900

    Chore: 로깅 작성

--- src/main/java/com/ll/hereispaw/domain/payment/payment/service/PaymentService.java
+++ src/main/java/com/ll/hereispaw/domain/payment/payment/service/PaymentService.java
@@ -10,6 +10,7 @@ import com.ll.hereispaw.global.exception.CustomException;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.json.simple.JSONObject;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
@@ -17,6 +18,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class PaymentService {
     private final PaymentRepository paymentRepository;
     @PersistenceContext
@@ -30,16 +32,19 @@ public class PaymentService {
         // state: "DONE" - 결제 완료
         String status = (String) responseData.get("status");
         if (!"DONE".equals(status)) {
+            log.error("Payment failed: status = " + status);
             throw new CustomException(ErrorCode.PAYMENT_FAILED);
         }
 
         // 결제 금액 추출
         long totalAmount = ((Long) responseData.get("totalAmount"));
         final Integer finalAmount = (int) totalAmount;
+        log.info("결제 금액 = " + finalAmount);
 
         // 중복 결제되었는지 확인
         String paymentKey = (String) responseData.get("paymentKey");
         if (isDuplicatePayment(paymentKey)) {
+            log.error("Duplicate payment: paymentKey = " + paymentKey);
             throw new CustomException(ErrorCode.DUPLICATE_PAYMENT);
         }
 
@@ -53,10 +58,11 @@ public class PaymentService {
 
             // 이벤트 발행
             eventPublisher.publishEvent(new PaymentConfirmedEvent(this, payment));
+            log.info("Payment saved: paymentKey = {}, amount = {}", paymentKey, finalAmount);
 
-            System.out.println("PaymentService.savePaymentData: payment amount = " + payment.getAmount());
             return paymentRepository.save(payment);
         } catch (Exception e) {
+            log.error("Failed to save payment: paymentKey = " + paymentKey);
             throw new CustomException(ErrorCode.PAYMENT_SAVE_FAILED);
         }
     }
--- src/main/java/com/ll/hereispaw/domain/payment/point/service/PointService.java
+++ src/main/java/com/ll/hereispaw/domain/payment/point/service/PointService.java
@@ -5,6 +5,7 @@ import com.ll.hereispaw.domain.payment.payment.entity.Payment;
 import com.ll.hereispaw.domain.payment.point.entity.Point;
 import com.ll.hereispaw.domain.payment.point.repository.PointRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,15 +13,18 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
+@Transactional(readOnly = true)
 public class PointService {
     private final PointRepository pointRepository;
 
-    @Transactional
     public void updatePoint(Payment payment) {
         Point userPoint = payment.getUserPoint();
         int pointAmount = payment.getAmount();
 
+        log.info("before point: " + userPoint.getPoints());
         userPoint.addPoints(pointAmount);
+        log.info("after point: " + userPoint.getPoints());
 
         pointRepository.save(userPoint);
     }
